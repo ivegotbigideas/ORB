f9eabd5778b52631f2944b552e485341ffb31562
Jonah Loughlin <officerKJ@protonmail.com>
2023-11-20 13:26:24 +0000

    formatting

--- scripts/api.py
+++ scripts/api.py
@@ -21,7 +21,7 @@ debug = False
 class Orb:
     def __init__(self):
         # state
-        self.robot_state = "searching" # searching, converging or terminated
+        self.robot_state = "searching"  # searching, converging or terminated
 
         # subscribers
         if debug:  # debug
@@ -42,8 +42,10 @@ class Orb:
             )
 
         # publishers
-        self._robot_twist_publisher = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
-        self._robot_model_state_publisher = rospy.Publisher("/gazebo/set_model_state", ModelState, queue_size=10)
+        self._robot_twist_publisher = rospy.Publisher("/cmd_vel", Twist, queue_size=10)
+        self._robot_model_state_publisher = rospy.Publisher(
+            "/gazebo/set_model_state", ModelState, queue_size=10
+        )
 
     def get_latest_camera_data(self, *callback_message):
         """
@@ -223,12 +225,12 @@ class Target:
             pass
         return get_true_pose(msg, target_name)
 
-
     def randomise_target_pose(self):
         """
         Randomise target pose
         """
         randomise_pose(target_name)
+
 
 def get_true_pose(model_states_msg, obj_name):
     """
@@ -251,11 +253,12 @@ def get_true_pose(model_states_msg, obj_name):
     }
     return pose
 
+
 def randomise_pose(obj_name):
     msg = ModelState()
     msg.model_name = obj_name
-    msg.pose.position.x = random.uniform(-5, 5) # tweak these limits
-    msg.pose.position.y = random.uniform(-5, 5) # teak these limits
+    msg.pose.position.x = random.uniform(-5, 5)  # tweak these limits
+    msg.pose.position.y = random.uniform(-5, 5)  # teak these limits
     msg.pose.position.z = 1
 
     new_yaw = random.uniform(-np.pi, np.pi)
@@ -266,6 +269,7 @@ def randomise_pose(obj_name):
     msg.pose.orientation.w = new_quat[3]
 
     rospy.ServiceProxy("/gazebo/set_model_state", SetModelState)(msg)
+
 
 rospy.init_node("bot_api")
 orb = Orb()
