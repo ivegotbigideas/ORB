b3e4002a449b8e078f11fa050d6a25025ca7b3c0
Jonah Loughlin <officerKJ@protonmail.com>
2023-11-22 21:22:33 +0000

    basic navigation to object

--- scripts/imaging_toolkit.py
+++ scripts/imaging_toolkit.py
@@ -6,14 +6,45 @@ orb = Orb()
 target = Target()
 rospy.init_node("image_toolkit")
 
-image_cv2_bgr = cv2.cvtColor(orb.get_latest_camera_data(), cv2.COLOR_RGB2BGR)
-cv2.imwrite("unmasked.png", image_cv2_bgr)
+def detect_target():
+    image_cv2_bgr = cv2.cvtColor(orb.get_latest_camera_data(), cv2.COLOR_RGB2BGR)
+    image_cv2_hsv = cv2.cvtColor(image_cv2_bgr, cv2.COLOR_BGR2HSV)
+    lower_pink = (140, 100, 100)
+    upper_pink = (170, 255, 255)
+    mask = cv2.inRange(image_cv2_hsv, lower_pink, upper_pink)
 
-image_cv2_hsv = cv2.cvtColor(image_cv2_bgr, cv2.COLOR_BGR2HSV)
-lower_pink = (140, 100, 100)
-upper_pink = (170, 255, 255)
-mask = cv2.inRange(image_cv2_hsv, lower_pink, upper_pink)
-image_cv2_masked = cv2.bitwise_and(image_cv2_hsv, image_cv2_hsv, mask=mask)
-cv2.imwrite("masked.png", image_cv2_masked)
+    contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
+    if contours:
+        max_contour = max(contours, key=cv2.contourArea)
+        x, y, width, height = cv2.boundingRect(max_contour)
+        return x, y, width, height
+    else:
+        return None, None, None, None
+
+def find_center_of_target(x, y, w, h):
+    middle_x = x + w // 2
+    middle_y = y + h // 2
+    return middle_x, middle_y
+
+def move_robot(horizontal_coord):
+    if horizontal_coord == None:
+        orb.move_robot("cw")
+    elif horizontal_coord in range(318, 323):
+        orb.move_robot("f")
+    elif horizontal_coord < 318:
+        orb.move_robot("acw")
+    elif horizontal_coord > 323:
+        orb.move_robot("cw")
+
+while True:
+    try:
+        x, y, width, height = detect_target()
+        if x == None:
+            move_robot(None)
+        else:
+            center = find_center_of_target(x, y, width, height)
+            move_robot(center[0])
+    except Exception as e:
+        print(e)
 
